ca8b39fe1b074520733e07ab2bc0492cbe6157f4
joel.hakansson@tpb.se <joel.hakansson@tpb.se@c96afea0-5d08-d5ca-c39d-2c9af40aa567>
2011-09-14 13:18:59 +0000

    new implementation of LocalhostSocketFilter
    git-svn-id: https://ajui.googlecode.com/svn/trunk@20 c96afea0-5d08-d5ca-c39d-2c9af40aa567

--- ajui/src/com/googlecode/ajui/LocalhostSocketFilter.java
+++ ajui/src/com/googlecode/ajui/LocalhostSocketFilter.java
@@ -1,25 +1,21 @@
 package com.googlecode.ajui;
 
+import java.net.InetAddress;
 import java.net.Socket;
-import java.util.regex.Pattern;
+import java.net.UnknownHostException;
 
 class LocalhostSocketFilter implements SocketFilter {
-	final static Pattern[] patterns;
-	
-	static {
-		patterns = new Pattern[3];
-		patterns[0] = Pattern.compile("127\\.0\\.0\\.1");
-		patterns[1] = Pattern.compile("0:0:0:0:0:0:0:1%0");
-		patterns[2] = Pattern.compile("::1");
-	}
 
 	@Override
 	public boolean accept(Socket s) {
-		String addr = s.getInetAddress().getHostAddress();
-		for (int i = 0; i<patterns.length; i++) {
-			if (patterns[i].matcher(addr).matches()) {
-				return true;
+		try {
+			for (InetAddress local : InetAddress.getAllByName("localhost")) {
+				if (local.equals(s.getInetAddress())) {
+					return true;
+				}
 			}
+		} catch (UnknownHostException e) {
+			// should not happen
 		}
 		return false;
 	}
